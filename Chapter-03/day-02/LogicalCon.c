c595d8f51b2b26a976d7cea637bcb3553b2e4a5b
JoneCoder <mohummadshorifulislam@gmail.com>
2022-03-22 00:19:49 +0600

    update day-06

---
+++ Chapter-03/day-02/LogicalCon.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    /*char ch;
+    printf("Enter a  Letter:");
+    scanf("%c", &ch);
+
+    // 1 >= 1  &&  26 >= 1
+    if(ch >= 'A' && 'Z' >= ch){
+        printf("%c is upper case\n", ch);
+    }
+
+    // -26 >= -26 && -1 >= -26
+    else if(ch >= 'a' && 'z' >= ch){
+        printf("%c is lower case\n", ch);
+    }
+
+    else{
+        printf("Invalid data!"); 
+    }
+
+    //A = 1, B = 2, C = 3 ...  ... Z = 26
+    //a = -26, b = -25, c = -24 ...  ... z = -1
+    */
+
+    
+
+   int num = 5;
+
+   if(num <= 1 || num <= 10){
+       printf("Yes\n");
+   }else{
+       printf("No\n");
+   }
+
+   //Logical Operator
+   /*
+
+   1. AND => &&
+   2. OR => ||
+
+   */
+
+
+    return 0;
+}
